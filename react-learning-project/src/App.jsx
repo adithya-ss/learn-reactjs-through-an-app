b9d37d2fb0e73c4137b34a05595f0ba126f2087f
Ss, Adithya <adithya.ss@philips.com>
2025-05-29 13:16:34 +0530

    Add like functionality and count display to Card component; remove StrictMode from main render

--- react-learning-project/src/App.jsx
+++ react-learning-project/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Card = ({ title }) => {
+    const [count, setCount] = useState(0); 
     const [hasLiked, setHasLiked] = useState(false);
+    useEffect(() => {
+        console.log(`"${title}" has been liked: ${hasLiked}.`);
+    }, [hasLiked]); // This will run every time hasLiked changes
+
+    /*
+    The below use effect will run only ONCE, when the component is mounted.App
+    useEffect(() => {
+        console.log("CARD RENDERED!");
+    }, []);
+    */
+
     return (
-        <div className='card'>
-            <h2>{title}</h2>
+        // <div className='card' onClick={() => setcount((prevState) => prevState + 1)}> ...... </div> - Recommended to use another callback function without having to use the state variable directly. 
+        <div className='card' onClick={() => setCount(count + 1)}>
+            <h2>{title} <br/> {count || null}</h2>
             <button onClick={() => setHasLiked(!hasLiked)}>
                 {hasLiked ? 'Liked' : 'Like'}
             </button>
